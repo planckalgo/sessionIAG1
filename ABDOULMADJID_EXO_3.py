5e0213fc0db9f32cd17d6ba9ade87e5ffc5acc40
planckalgo <96715195+planckalgo@users.noreply.github.com>
2022-03-21 10:22:03 +0100

    Add files via upload

---
+++ ABDOULMADJID_EXO_3.py
@@ -0,0 +1,7 @@
+#table demultiplication de 7
+for i in range(1,21):
+    p=7*i
+    print(7,'*',i,'=',p)
+Tf=float(input(print ("Entrez la temperature en degre Fahrenheit")))
+Tc=float((Tf-32)/1.8)
+print('La temperature en degre est:',Tc,'°C')
---
+++ ABDOULMADJID_EXO_4.py
@@ -0,0 +1,15 @@
+Capital=100
+Taux=0.043
+Duree=20
+Intpre=0
+for i in range(1,20):
+    interet=float((Capital*Taux)+Intpre)
+    Intpre=interet
+print('InCapital=100
+Taux=0.043
+Duree=20
+Intpre=0
+for i in range(1,20):
+    interet=float((Capital*Taux)+Intpre)
+    Intpre=interet
+print('Interet accumulé pendant 20 ans: ',interet,' Euro')
